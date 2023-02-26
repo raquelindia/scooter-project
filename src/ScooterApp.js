d7901e27b90e64366f616ed7027edf2fc9b8f3e6
raquelindia <raquelicruz94@gmail.com>
2023-02-26 10:48:32 -0500

    added registerUser method

--- src/ScooterApp.js
+++ src/ScooterApp.js
@@ -9,8 +9,24 @@ class ScooterApp {
 
   }
 registerUser(username, password, age){
+ let count = 0;
+ for (let i =  0; i < this.registeredUsers; i++){
+  if (registeredUsers[i] === username){
+    count++;
+  }
+ }
+ if(count > 0){
+  console.log('already registered');
+} 
+if (age < 18){
+  throw new Error('too young to register')
+} 
+if (count === 0 && age >= 18){
+  console.log('user has been registered')
+}
 
 }
+ }
 loginUser(username, password){
 
 }
@@ -20,11 +36,24 @@ logoutUser(username){
 }
 
 createScooter(station){
+  if (!station){
+    throw new Error('no such station');
+  }
 
 }
 
 dockScooter(scooter, station){
+  if(!station){
+    throw new Error('no such station')
+  }
+  for (let i = 0; i < station.length; i++){
+    if (station[i] === scooter){
+      throw new Error('scooter already at station')
+    }
+  }
 
+ station.push(scooter);
+ console.log('scooter is docked')
 }
 
 rentScooter(scooter, user){
